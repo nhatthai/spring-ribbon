6f2ec13979180a69df8895da8c9a6cf99da18d10
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2018-02-23 08:13:45 +0700

    Remove comment out

--- user-client-service/src/main/java/hello/UserClient.java
+++ user-client-service/src/main/java/hello/UserClient.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +8,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import feign.RequestLine;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+
+// public interface UserClient {
+//     @RequestLine("GET /hi_getall")
+//     List<String> getGreetings();
+// }
+
+@FeignClient(name = "say-hello", fallback = UserClientFallback.class)
 public interface UserClient {
     @RequestLine("GET /hi_getall")
     List<String> getGreetings();
+}
+
+@Component
+class UserClientFallback implements UserClient {
+    @Override
+    public List<String> getGreetings() {
+        return Arrays.asList("Hello", "tryGetGreetingsElse");
+    }
 }
